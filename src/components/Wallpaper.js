da2c0dc7740624c475e8ebb249972efe043f3309
kbudhwar <khushi.budhwar@gmail.com>
2022-06-13 16:28:58 -0400

    fixed background

--- src/components/Wallpaper.js
+++ src/components/Wallpaper.js
@@ -1,12 +1,23 @@
-import React from "react";
-import videoBG from "./../assests/Background.mp4";
+import React, { Component } from "react";
+import { render } from "react-dom";
+import ParticlesBg from "particles-bg";
+import "./style.css";
 
-const Wallpaper = () => {
-  return (
-    <div className="GradientVideo">
-      <video src={videoBG} autoPlay loop muted />
-    </div>
-  );
-};
+class Wallpaper extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <ParticlesBg num={100} type="circle" bg={true} />
+      </div>
+    );
+  }
+}
 
 export default Wallpaper;
