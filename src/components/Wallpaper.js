1f97ede1a19ad479760c750f3c447f5b1976d792
kbudhwar <khushi.budhwar@gmail.com>
2022-06-13 20:38:41 -0400

    title

--- src/components/Wallpaper.js
+++ src/components/Wallpaper.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
 import ParticlesBg from "particles-bg";
 import "./style.css";
+import TypeWriter from "react-typewriter-effect";
 
 class Wallpaper extends Component {
   constructor() {
@@ -15,6 +15,20 @@ class Wallpaper extends Component {
     return (
       <div>
         <ParticlesBg num={100} type="circle" bg={true} />
+        <div className="title">
+          <TypeWriter
+            textStyle={{
+              fontFamily: "Nunito",
+              color: "#ffffff",
+              fontWeight: 1000,
+              fontSize: "4em",
+            }}
+            startDelay={100}
+            cursorColor="white"
+            text="Welcome to Name!"
+            typeSpeed={50}
+          />
+        </div>
       </div>
     );
   }
