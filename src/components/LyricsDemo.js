54d07a2b2a18f19688f99807244cf247324cb577
kbudhwar <khushi.budhwar@gmail.com>
2022-06-26 21:42:27 -0400

    adding the song and artist

--- src/components/LyricsDemo.js
+++ src/components/LyricsDemo.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import ParticlesBg from "particles-bg";
-import "../assests/style.css";
+import "../assests/lyrics.css";
 import { useLocation } from "react-router-dom";
 
 export default function LyricsDemo() {
   const [lyrics, setLyrics] = useState("");
+  const [artistPrint, setArtist] = useState("");
+  const [namePrint, setName] = useState("");
 
   const location = useLocation();
   const playlist = location.state.playlists;
+  let name = "";
+  let artist = "";
   useEffect(() => {
     const getTrack = async () => {
-      let name, artist;
       let tracks = [];
       await Axios.get("http://localhost:8000/spotify/getPlaylistTracks", {
         params: {
@@ -23,9 +26,13 @@ export default function LyricsDemo() {
           tracks = res.data;
         })
         .catch((err) => console.log(err));
+
       let x = Math.floor(Math.random() * 100);
       artist = tracks.tracks[x].track.artists[0].name;
       name = tracks.tracks[x].track.name;
+      setArtist(artist);
+      setName(name);
+
       await Axios.get("http://localhost:8000/spotify/lyrics", {
         params: {
           name: name,
@@ -40,6 +47,10 @@ export default function LyricsDemo() {
   return (
     <div>
       <ParticlesBg num={100} type="circle" bg={true} />
+      <attributes>
+        {namePrint}
+        {" by"} {artistPrint}
+      </attributes>
       <pre>{lyrics}</pre>
     </div>
   );
